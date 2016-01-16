ccc437b10cd17b099e4146590b43bad5c0ec216e
xboxfanj <xboxlover360@gmail.com>
2016-01-16 18:45:48 +0100

    X00T: Clean up kernel cmdline
     * Console is overwritten by the bootloader
     * CONFIG_DEBUG_USER is disabled, so no need to keep user_debug
       and the same goes for msm_rtb, which depends on CONFIG_MSM_RTB
    
    Change-Id: I9a30f32f7dc91cf82f2516bf6a54d7d62f1c00f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,8 +49,7 @@ TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000
-BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=1 androidboot.configfs=true
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a800000.dwc3 firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
